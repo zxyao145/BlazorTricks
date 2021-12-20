e11311f329b704cbc4103ee0e2cccee265e2c2f2
zxyao <zxyao145@gmail.com>
2021-12-20 22:26:09 +0800

    添加项目文件。

---
+++ Stateless/Model/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Stateless.Model
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ Stateless/Utils.cs
@@ -0,0 +1,9 @@
+﻿namespace Stateless
+{
+    public class Utils
+    {
+        public static List<long> DisplayPersonFullTimes = new List<long>(10000);
+
+        public static List<long> DisplayPersonTimes = new List<long>(10000);
+    }
+}
